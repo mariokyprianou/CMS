adfa2b74591322c6b39ca7328450b758f09e323a
Harry Crank <harry.crank@thedistance.co.uk>
2020-11-24 08:27:06 +0000

    feat(PDL-270): User Edit screen created.

--- src/Resources/User/edit.js
+++ src/Resources/User/edit.js
@@ -1,14 +1,59 @@
+/*
+ * Jira Ticket: PDL-270
+ * Created Date: Mon, 23rd Nov 2020, 14:24:39 pm
+ * Author: Harry Crank
+ * Email: harry.crank@thedistance.co.uk
+ * Copyright (c) 2020 The Distance
+ */
+
 import React from 'react';
-import { Edit, SimpleForm, TextField } from 'react-admin';
+import {
+  Edit,
+  SimpleForm,
+  TextField,
+  TextInput,
+  BooleanInput,
+  SelectInput,
+} from 'react-admin';
+import { columnStyles } from 'styles';
+import { subscriptionPlatformChoices } from 'utils/choices';
 
-const UserList = (props) => (
-  <Edit {...props}>
-    <SimpleForm>
-      <TextField source="id" />
-      <TextField source="name" />
-      <TextField source="role" />
-    </SimpleForm>
-  </Edit>
-);
+// TODO: Update the sources for the second column of inputs.
+// https://thedistance.atlassian.net/wiki/spaces/PDL/pages/2026242085/2.1+User+Management
 
-export default UserList;
+const UserEdit = (props) => {
+  const classes = columnStyles();
+
+  return (
+    <Edit {...props}>
+      <SimpleForm>
+        <div className={classes.root}>
+          <div className={classes.column}>
+            <TextInput source="givenName" />
+            <TextInput source="familyName" />
+            <TextInput source="email" />
+            <TextInput source="country" />
+            <TextInput
+              // TODO: Should only appear if Country === India
+              source="region"
+            />
+            <BooleanInput source="subscription.isSubscribed" disabled />
+            <SelectInput
+              source="subscription.platform"
+              choices={subscriptionPlatformChoices}
+            />
+          </div>
+          <div className={classes.column}>
+            <TextInput source="currentTrainer" />
+            <TextInput source="currentWeek" />
+            <TextInput source="previousTrainers" multiline disabled />
+            <TextInput source="deviceLimit" />
+            <TextInput source="timeZone" />
+          </div>
+        </div>
+      </SimpleForm>
+    </Edit>
+  );
+};
+
+export default UserEdit;
