6c3bef749f573be09678f179e7795a85bdc0e324
Jessica Mowatt <jessica.mowatt@thedistance.co.uk>
2021-04-23 13:24:54 +0100

    fix: countries list perPage 25 -> 500 [PDL-850]

--- src/Resources/User/edit.js
+++ src/Resources/User/edit.js
@@ -80,6 +80,7 @@ const SanitizedForm = ({ basePath, classes, ...props }) => {
           resource={resource}
           reference="country"
           source="country.id"
+          perPage={500}
           sort={{ field: 'name', order: 'ASC' }}
           validate={required()}
         >
